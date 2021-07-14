e7754b3039b89e9ce43625fa304e38f9c723e0fd
Maximilian Konrad <maximilianlukaskonrad@hotmail.de>
2021-07-14 13:42:38 +0200

    Added: Button Design and Auth page

---
+++ src/views/Auth.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+      <div id="login">
+        <h1>This is an example of the Authorization Code flow</h1>
+        <a href="/login" class="btn btn-primary">Log in with Spotify</a>
+      </div>
+      <div id="loggedin">
+        <div id="user-profile">
+        </div>
+        <div id="oauth">
+        </div>
+        <button class="btn btn-default" id="obtain-new-token">Obtain new token using the refresh token</button>
+      </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'AuthView'
+}
+</script>
+
+<style scoped lang="scss">
+#login, #loggedin {
+display: none;
+}
+.text-overflow {
+overflow: hidden;
+text-overflow: ellipsis;
+white-space: nowrap;
+width: 500px;
+      }
+</style>
